935bff2e9b14672595c5f20ad5f8c0dbb53d3914
alvaromm14 <49958868+alvaromm14@users.noreply.github.com>
2024-10-09 12:08:19 +0200

    Add files via upload

--- src/App.svelte
+++ src/App.svelte
@@ -39,7 +39,7 @@
 </script>
 
 <div class="main" bind:clientWidth={width}>
-<h1>Años de ahorro necesarios del hogar mediano para pagar una vivienda media</h1>
+<h1>Años de sueldo del hogar mediano necesarios para comprar una vivienda</h1>
 <div class="chart-container">
   <svg {width} {height}>
     <g class="inner-chart" transform="translate({margin.left}, {margin.top})">
